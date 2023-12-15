54b505349337a534bb9b6e67e980dd8a47e9c6f8
Henok Tollosa <henoktollosa00@gmail.com>
2023-12-15 12:36:39 -0800

    Dynamic task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int add(int a , int b){
+        return a+b;
+}
+
+
+int sub(int a , int b){
+        return a-b;
+}
+
+int multi(int a , int b){
+        return a*b;
+}
+
+int divd(int a , int b){
+        if (b==0){
+        return 0,
+}
+int mod (int a, int b) {
+        if (b==0) {
+                printf("Error");
+        }
+        return a%b;
+}
